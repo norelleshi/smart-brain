1a7b469a6bd59ab593c6784c91dbfa8b21fb80b4
Ying-Li Shih <norelle@Ying-Lis-MacBook-Pro.local>
2019-11-18 23:39:49 +0800

    Updates

---
+++ precache-manifest.f5db582eb4ac8ebcb6e0788d3d8cbb12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e980044be51e7d8a03843e7c4ada9ea5",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "997f97359f52b389cb02",
+    "url": "/smart-brain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b7e221b8305e34bd4367",
+    "url": "/smart-brain/static/css/main.a50f7e8e.chunk.css"
+  },
+  {
+    "revision": "997f97359f52b389cb02",
+    "url": "/smart-brain/static/js/2.275979c1.chunk.js"
+  },
+  {
+    "revision": "b7e221b8305e34bd4367",
+    "url": "/smart-brain/static/js/main.41b829d9.chunk.js"
+  },
+  {
+    "revision": "f964384144b3df66f40f",
+    "url": "/smart-brain/static/js/runtime-main.b58abc41.js"
+  },
+  {
+    "revision": "5249cbfef79ae03173cdcd6eca051a0b",
+    "url": "/smart-brain/static/media/logo.5249cbfe.png"
+  }
+]);
